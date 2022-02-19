99334f97fab751f743a1e12e1938d07a802540d1
ElnurKasimov <ekasimov2012@gmail.com>
2022-02-19 07:51:10 +0200

    добавлены в MyArray List методы add(), remove(), clear(), get(int index)

--- CollectionsTesting.java
+++ CollectionsTesting.java
@@ -1,10 +1,37 @@
 package Collections;
 
+import java.util.Iterator;
+
 public class CollectionsTesting {
 
     public static void main(String[] args) {
-        MyArrayList <Integer> myArrayList = new MyArrayList<Integer>();
-        System.out.println(myArrayList.toString());
-        System.out.println(myArrayList.size());
+
+       // проверка на Integer  и методов toString() , add() и remove()
+
+        MyArrayList <Integer> myArrayList1 = new MyArrayList<Integer>();
+        System.out.println(myArrayList1.toString()); // проврка на нулевой массив
+        for (int i = 0; i < 35; i++) {
+            myArrayList1.add(i);
+        }
+        System.out.println(myArrayList1.toString());
+        myArrayList1.remove(0);
+        myArrayList1.remove(34);
+        System.out.println(myArrayList1.toString());
+
+        //  проверка на String и методов  get() , size() и remove()
+        MyArrayList <String> myArrayList2 = new MyArrayList<String>();
+        myArrayList2.add("проверка");
+        myArrayList2.add("на");
+        myArrayList2.add("работоспособность");
+        myArrayList2.add("обработки");
+        myArrayList2.add("строк");
+        for (int i=0; i < myArrayList2.size(); i++) {
+            System.out.print(myArrayList2.get(i) + ", ");
+        }
+        System.out.println("\n" + myArrayList2.toString());
+        myArrayList2.clear();
+        System.out.println( myArrayList2.toString());
+
+    //  System.out.println("заполненно - " + myArrayList1.size() + " ячеек из " + myArrayList1.getValues().length);
     }
 }
--- MyArrayList.java
+++ MyArrayList.java
@@ -1,12 +1,14 @@
 package Collections;
 
+import java.util.Arrays;
+
 public class MyArrayList <T>{
-   private final int size;
-   private final T [] values;
+   private int size;
+   private T [] values;
 
     public MyArrayList() {
         this.size = 0;
-        this.values =  (T[]) new Object [50];
+        this.values =  (T[]) new Object [30];
     }
 
     public int getSize() {
@@ -17,24 +19,59 @@ public class MyArrayList <T>{
         return values;
     }
 
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setValues(T[] values) {
+        this.values = values;
+    }
+
+    public void add(T t) {
+        // надо бы добавить проверку на соответствие типа
+        // а то компилятор прерывает выпонение если идет несооответствие
+        if (size == values.length) {
+            int newsize = (int) (size*1.5) ;
+            T[] temp = (T[]) new Object [newsize];
+            temp =  Arrays.copyOf(values, newsize);
+            this.setValues(temp);
+        }
+        values[size] = (T) t;
+        size++;
+    }
+
+    public void remove (int index ) {
+        for (int i = index; i < size ; i++) {
+            values[i] = values[i+1];
+        }
+        values[size] = null;
+        size--;
+    }
+
+public void clear () {
+    for (int i = 0; i < size; i++) values[i] = null;
+    size=0;
+}
+
     public int size() {
         return size;
     }
+
+    public  T get(int index) { return values[index];}
+
 
     public String toString(){
         if (this.getSize() == 0) {return "[null]";}
         else {
         String result = "[";
-        for (int i = 0; i < this.getSize() - 1; i++) {
-            result += this.getValues()[i];
-            if (i != this.getSize() - 1) {
+        for (int i = 0; i < size ; i++) {
+            result += values[i];
+            if (i != size - 1) {
                 result += ", ";
             }
         }
         return result+"]";
         }
     }
-
-
 
 }
